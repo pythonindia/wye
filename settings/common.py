e5e3fedfdd4fb9255ec12dbb24e75786f509af88
vijay <vnbang2003@gmail.com>
2015-08-26 22:51:38 +0530

    Added Settings
    Added Settings

--- settings/common.py
+++ settings/common.py
@@ -30,7 +30,7 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = (
+DEFAULT_APPS = (
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -38,6 +38,15 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
 )
+
+OUR_APPS = (
+    'wye.profiles',
+    'wye.organisations',
+    'wye.workshops',
+)
+
+INSTALLED_APPS = DEFAULT_APPS + OUR_APPS
+
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
