e2c09fc88aeeb9f3ec3a5be407ca2d4d0a70eb1c
Deshraj <deshrajdry@gmail.com>
2015-09-27 00:51:59 +0530

    urls config changed for integrating django-allauth

--- wye/urls.py
+++ wye/urls.py
@@ -6,15 +6,10 @@ from django.views.generic.base import TemplateView
 
 
 urlpatterns = [
+    url(r'', include('allauth.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^about/$', TemplateView.as_view(template_name='about.html',),
         name='about'),
-    url(r'^signup/$', TemplateView.as_view(template_name='auth/signup.html',),
-        name='login'),
-    url(r'^login/$', TemplateView.as_view(template_name='auth/login.html',),
-        name='login'),
-    url(r'^logout/$', TemplateView.as_view(template_name='auth/logout.html',),
-        name='logout'),
     url(r'^$', TemplateView.as_view(template_name='index.html',),
         name='home-page'),
 ] + static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS)
