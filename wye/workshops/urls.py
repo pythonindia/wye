acc88040b5c20b1584f4c5ca5b0d59f8ad9363f7
vijay <vnbang2003@gmail.com>
2015-10-12 22:36:48 +0530

    Added / as end character in url
    Added / as end character in url

--- wye/workshops/urls.py
+++ wye/workshops/urls.py
@@ -4,9 +4,10 @@ from .views import WorkshopList, WorkshopDetail, \
 
 urlpatterns = [
     url(r'^$', WorkshopList.as_view(), name="workshop_list"),
-    url(r'^(?P<pk>\d+)$', WorkshopDetail.as_view(), name="workshop_detail"),
-    url(r'^create$', WorkshopCreate.as_view(), name="workshop_create"),
-    url(r'^update/(?P<pk>\d+)$', WorkshopUpdate.as_view(), name="workshop_update"),
-    url(r'^(?P<action>[active,deactive]+)/(?P<pk>\d+)$', 
+    url(r'^(?P<pk>\d+)/$', WorkshopDetail.as_view(), name="workshop_detail"),
+    url(r'^create/$', WorkshopCreate.as_view(), name="workshop_create"),
+    url(r'^update/(?P<pk>\d+)/$',
+        WorkshopUpdate.as_view(), name="workshop_update"),
+    url(r'^(?P<action>[active,deactive]+)/(?P<pk>\d+)/$',
         WorkshopToggleActive.as_view(), name="workshop_toggle"),
 ]
