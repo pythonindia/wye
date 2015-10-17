a4be7eb2fc5e959fc7c91bb168befc6ba7c836a9
Praseetha-KR <voidimagineer@yahoo.com>
2015-10-17 21:24:58 +0530

    Use scss instead of sass

--- gulpfile.js
+++ gulpfile.js
@@ -6,16 +6,15 @@ var autoprefixer = require('gulp-autoprefixer');
 var sourcemaps = require('gulp-sourcemaps');
 
 var paths = {
-    sass: 'wye/static/sass/*.sass',
+    sass: 'wye/static/sass/*.scss',
     css: 'wye/static/css/*.css'
 };
 
 gulp.task('css', function() {
     return gulp.src(paths.sass)
         .pipe(sourcemaps.init())
-            .pipe(sass({
-                indentedSyntax: true
-            }).on('error', sass.logError))
+            .pipe(sass()
+                .on('error', sass.logError))
             .pipe(autoprefixer({
                 browsers: ['> 1%']
             }))
