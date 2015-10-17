d82c175c6bb552939b8ef65558330ac88243176b
Praseetha-KR <voidimagineer@yahoo.com>
2015-10-17 20:28:47 +0530

    Add gulp workflow for sass compile/minify/autoprefix/sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,33 @@
+'use strict';
 var gulp = require('gulp');
+var sass = require('gulp-sass');
+var minifycss = require('gulp-minify-css');
+var autoprefixer = require('gulp-autoprefixer');
+var sourcemaps = require('gulp-sourcemaps');
 
-gulp.task('default', function() {
-    //
+var paths = {
+    sass: 'wye/static/sass/*.sass',
+    css: 'wye/static/css/*.css'
+};
+
+gulp.task('css', function() {
+    return gulp.src(paths.sass)
+        .pipe(sourcemaps.init())
+            .pipe(sass({
+                indentedSyntax: true
+            }).on('error', sass.logError))
+            .pipe(autoprefixer({
+                browsers: ['> 1%']
+            }))
+            .pipe(minifycss({
+                debug: true
+            }))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest(paths.css));
 });
+
+gulp.task('watch', function() {
+    gulp.watch(paths.sass, ['css']);
+});
+
+gulp.task('default', ['watch']);
