a7efbb4e46cd02590439187a997139bf2601c40e
Shanki <ssg.bnv@gmail.com>
2015-10-18 23:01:08 +0530

    fix urls and constants

--- wye/workshops/urls.py
+++ wye/workshops/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
 from .views import WorkshopList, WorkshopDetail, \
-    WorkshopCreate, WorkshopUpdate, WorkshopToggleActive
+    WorkshopCreate, WorkshopUpdate, WorkshopToggleActive, \
+    WorkshopAssignMe
+
 
 urlpatterns = [
     url(r'^$', WorkshopList.as_view(), name="workshop_list"),
@@ -8,6 +10,8 @@ urlpatterns = [
     url(r'^create/$', WorkshopCreate.as_view(), name="workshop_create"),
     url(r'^update/(?P<pk>\d+)/$',
         WorkshopUpdate.as_view(), name="workshop_update"),
-    url(r'^(?P<action>[active,deactive]+)/(?P<pk>\d+)/$',
+    url(r'^(?P<pk>\d+)/(?P<action>active|deactive)/$',
         WorkshopToggleActive.as_view(), name="workshop_toggle"),
+    url(r'^(?P<pk>\d+)/(?P<action>opt-in|opt-out)/$',
+        WorkshopAssignMe.as_view(), name="workshop_assignme")
 ]
