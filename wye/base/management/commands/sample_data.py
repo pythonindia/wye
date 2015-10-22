cb57272ae5d250a60e8e76b0fc754e548d10cf89
ChillarAnand <anand21nanda@gmail.com>
2015-10-22 15:08:01 +0530

    Create sample users after superuser

--- wye/base/management/commands/sample_data.py
+++ wye/base/management/commands/sample_data.py
@@ -1,9 +1,15 @@
 # -*- coding: utf-8 -*-
+from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.contrib.sites.models import Site
 from django.core.management.base import BaseCommand
 from django.db import transaction
 from faker import Faker
+
+from wye.organisations.models import Organisation, Location, State
+
+
+NUMBER_OF_USERS = getattr(settings, "NUMBER_OF_USERS", 10)
 
 
 class Command(BaseCommand):
@@ -18,10 +24,13 @@ class Command(BaseCommand):
         site.domain, site.name = 'localhost:8000', 'Local'
         site.save()
 
-        print('  Creating Superuser')  # create superuser
+        print('  Creating Superuser')
         self.create_user(is_superuser=True, username='admin', email='admin@fossevents.in',
                          is_active=True, is_staff=True, first_name='Admin')
 
+        print('  Creating sample users')
+        for i in range(NUMBER_OF_USERS):
+            self.create_user()
     def create_user(self, counter=None, **kwargs):
         params = {
             "first_name": kwargs.get('first_name', self.fake.first_name()),
@@ -32,13 +41,16 @@ class Command(BaseCommand):
             "is_superuser": kwargs.get('is_superuser', False),
             "is_staff": kwargs.get('is_staff', kwargs.get('is_superuser', self.fake.boolean())),
         }
-        user = get_user_model().objects.create(**params)
-        password = '123123'
 
-        user.set_password(password)
-        user.save()
+        user, created = get_user_model().objects.get_or_create(**params)
 
-        print("User created with username: {username} and password: {password}".format(
-              username=params['username'], password=password))
+        if params['is_superuser']:
+            password = '123123'
+            user.set_password(password)
+            user.save()
+
+            print("SuperUser created with username: {username} and password: {password}".format(
+                username=params['username'], password=password)
+            )
 
         return user
