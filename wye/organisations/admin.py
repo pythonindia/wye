f53f6ebb073e461d69866d136f321676cf3d2220
curioswati <jaiswalswati94@gmail.com>
2015-10-23 16:54:31 +0530

    edits to meet flake8 guidelines

--- wye/organisations/admin.py
+++ wye/organisations/admin.py
@@ -2,19 +2,21 @@ from django.contrib import admin
 
 from .models import Organisation
 
+
 class OrganisationAdmin(admin.ModelAdmin):
-	list_per_page = 10
-	list_display = (
-		'organisation_type',
-		'name',
-		'location')
-	search_fields = (
-		'organisation_type',
-		'name')
-	list_filter = (
-		'active',
-		'organisation_type',
-		'name',
-		'location')
+    list_per_page = 10
+    list_display = (
+        'organisation_type',
+        'name',
+        'location')
+    search_fields = (
+        'organisation_type',
+        'name')
+    list_filter = (
+        'active',
+        'organisation_type',
+        'name',
+        'location')
+
 
 admin.site.register(Organisation, OrganisationAdmin)
--- wye/regions/admin.py
+++ wye/regions/admin.py
@@ -2,19 +2,21 @@ from django.contrib import admin
 
 from .models import Location, State
 
+
 class LocationAdmin(admin.ModelAdmin):
-	list_per_page = 10
-	list_display = (
-		'name',
-		'state')
-	search_fields = ('name',)
-	list_filter = (
-		'name',
-		'state')
+    list_per_page = 10
+    list_display = (
+        'name',
+        'state')
+    search_fields = ('name',)
+    list_filter = (
+        'name',
+        'state')
+
 
 class StateAdmin(admin.ModelAdmin):
-	list_per_page = 10
-	search_fields = ('name',)
+    list_per_page = 10
+    search_fields = ('name',)
 
 admin.site.register(Location, LocationAdmin)
 admin.site.register(State, StateAdmin)
