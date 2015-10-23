cc4abf55fe0004d4f6fb836710fa355548945e71
Umang Shukla <umang.shukla@kuliza.com>
2015-10-23 19:56:02 +0530

    pep8 changes

--- wye/base/views.py
+++ wye/base/views.py
@@ -5,11 +5,12 @@ from wye.workshops.models import Workshop
 from wye.profiles.models import Profile
 from .constants import WorkshopStatus
 
+
 class HomePageView(TemplateView):
     template_name = 'index.html'
 
     def get_context_data(self, **kwargs):
-        context =  super(HomePageView, self).get_context_data(**kwargs)
+        context = super(HomePageView, self).get_context_data(**kwargs)
         context['organisation_registered_count'] = Organisation.objects.filter(active=True).count()
         context['completed_workshop_count'] = Workshop.objects.filter(status=WorkshopStatus.COMPLETED).count()
         context['tutor_registered_count'] = Profile.objects.filter(usertype__slug="tutor").count()
