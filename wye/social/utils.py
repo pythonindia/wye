64a282d532dce83cf5a40581664b3f66598e9e16
Haris Ibrahim K. V <blucalvin@gmail.com>
2015-10-25 16:01:18 +0530

    Adds the get and construct message functions

---
+++ wye/social/utils.py
@@ -0,0 +1,21 @@
+def get_message(context):
+    workshop = context.get('workshop', None)
+
+    if workshop:
+        return construct_message(workshop, context)
+    else:
+        # Other messages for non-workshop creation actions
+        return None
+
+def construct_message(workshop, context):
+    date = workshop.expected_date
+    topic = workshop.workshop_section
+    organization = workshop.requester
+    workshop_url = context.get('workshop_url', None)
+    message = "{} workshop at {} on {} confirmed! Details at {}".format(
+        topic, organization, date, workshop_url)
+    if len(message) >= 140:
+        message = "{} workshop on {} confirmed! Details at {}".format(
+            topic, date, workshop_url)
+
+    return message
