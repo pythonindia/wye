0afe3dd2b2f67af7ef82181a309dc9c377e24890
curioswati <jaiswalswati94@gmail.com>
2015-11-08 20:56:57 +0530

    optimizes code of previous commit

--- wye/workshops/templatetags/workshop_action_button.py
+++ wye/workshops/templatetags/workshop_action_button.py
@@ -5,29 +5,21 @@ register = template.Library()
 
 
 def show_draft_button(workshop, user):
-    if workshop.status in [WorkshopStatus.REQUESTED,
-                           WorkshopStatus.ACCEPTED,
-                           WorkshopStatus.DECLINED]:
-
-        requester_members = workshop.requester.user.values()
-
-        for member in requester_members:
-            if user.username == member['username']:
-                return True
-
+    if (workshop.status in [WorkshopStatus.REQUESTED,
+                            WorkshopStatus.ACCEPTED,
+                            WorkshopStatus.DECLINED]
+            and user in workshop.requester.user.all()):
+        return True
     return False
+
 register.filter(show_draft_button)
 
 
 def show_requested_button(workshop, user):
-    if workshop.status == WorkshopStatus.HOLD:
+    if (workshop.status == WorkshopStatus.HOLD
+            and user in workshop.requester.user.all()):
 
-        requester_members = workshop.requester.user.values()
-
-        for member in requester_members:
-            if user.username == member['username']:
-                return True
-
+        return True
     return False
 
 register.filter(show_requested_button)
@@ -42,44 +34,22 @@ register.filter(show_accepted_button)
 
 
 def show_feedback_button(workshop, user):
+    if (workshop.status == WorkshopStatus.COMPLETED
+            and (user in workshop.requester.user.all()
+                 or user in workshop.presenter.all())
+            and datetime.now().date() > workshop.expected_date):
 
-    is_requester = False
-    is_presenter = False
-    is_completed = False
-
-    if workshop.status == WorkshopStatus.COMPLETED:
-
-        requester_members = workshop.requester.user.values()
-        presenters = workshop.presenter.values()
-
-        for member in requester_members:
-            if user.username == member['username']:
-                is_requester = True
-
-        for presenter in presenters:
-            if user.username == presenter['username']:
-                is_presenter = True
-
-        if datetime.now().date() > workshop.expected_date:
-            is_completed = True
-
-        if is_completed and (is_requester or is_presenter):
-            return True
-
+        return True
     return False
 
 register.filter(show_feedback_button)
 
 
 def show_decline_button(workshop, user):
-    if workshop.status == WorkshopStatus.ACCEPTED:
+    if (workshop.status == WorkshopStatus.ACCEPTED
+            and user in workshop.presenter.all()):
 
-        presenters = workshop.presenter.values()
-
-        for presenter in presenters:
-            if user.username == presenter['username']:
-                return True
-
+        return True
     return False
 
 register.filter(show_decline_button)
