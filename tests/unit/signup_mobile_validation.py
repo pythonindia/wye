dd6fdd556dc5953d9ec297f29a5c5d58abe8055c
stalwartrishabh <0601rishabh@gmail.com>
2016-01-02 12:24:09 +0530

    PEP8 changes

--- tests/unit/signup_mobile_validation.py
+++ tests/unit/signup_mobile_validation.py
@@ -2,12 +2,12 @@ from wye.profiles.forms import SignupForm
 
 
 def test_invalid_mobile_number1():
-    mobile = SignupForm({'mobile' : '123$%78@0j'})
+    mobile = SignupForm({'mobile': '123$%78@0j'})
     mobile.is_valid()
     assert "Contact Number should only consist digits" in (mobile.errors['mobile'])
 
+
 def test_invalid_mobile_number2():
-    mobile = SignupForm({'mobile' : '12345'})
+    mobile = SignupForm({'mobile': '12345'})
     mobile.is_valid()
     assert "Contact Number should be of 10 digits" in (mobile.errors['mobile'])
-
