1ffb4acba44ab71abd9b84b6c68a6f73146f3c19
stalwartrishabh <0601rishabh@gmail.com>
2016-01-02 12:09:49 +0530

    renamed test cases as convention for py.test

--- tests/unit/signup_mobile_validation.py
+++ tests/unit/signup_mobile_validation.py
@@ -1,12 +1,12 @@
 from wye.profiles.forms import SignupForm
 
 
-def testcase1():
+def test_invalid_mobile_number1():
     mobile = SignupForm({'mobile' : '123$%78@0j'})
     mobile.is_valid()
     assert "Contact Number should only consist digits" in (mobile.errors['mobile'])
 
-def testcase2():
+def test_invalid_mobile_number2():
     mobile = SignupForm({'mobile' : '12345'})
     mobile.is_valid()
     assert "Contact Number should be of 10 digits" in (mobile.errors['mobile'])
