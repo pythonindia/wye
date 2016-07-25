3b21c1f7f721e4d3ba1fe0a50f114781ced7c933
vijay <vnbang2003@gmail.com>
2016-07-26 00:16:29 +0530

    Added more special character for profile slug
    Added more special character for profile slug

--- wye/profiles/urls.py
+++ wye/profiles/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     url(r'^dashboard$', views.UserDashboard.as_view(),
         name="dashboard"),
-    url(r'^(?P<slug>[a-zA-Z0-9@.]+)/$',
+    url(r'^(?P<slug>[a-zA-Z0-9@._-]+)/$',
         views.profile_view, name='profile-page'),
     url(r'^(?P<slug>[a-zA-Z0-9@._-]+)/edit$',
         views.ProfileEditView.as_view(), name='profile-edit')
