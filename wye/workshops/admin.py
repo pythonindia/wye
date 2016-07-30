6fe905f4043701f1c3abee664938dfdde1d77764
vijay <vnbang2003@gmail.com>
2016-07-30 11:55:39 +0530

    Add python express vector files
    Add python express vector files

--- wye/workshops/admin.py
+++ wye/workshops/admin.py
@@ -2,26 +2,26 @@ from django.contrib import admin
 from . import models
 
 
-class WorkshopAdmin(admin.ModelAdmin):
-    list_per_page = 50
-    list_display = (
-        'requester',
-        'location',
-        'workshop_section',
-        'no_of_participants')
-    search_fields = (
-        'expected_date',
-        'workshop_level',
-        'status')
-    list_filter = ('is_active', 'status', 'workshop_level', 'location')
+# class WorkshopAdmin(admin.ModelAdmin):
+#     list_per_page = 50
+#     list_display = (
+#         'requester',
+#         'location',
+#         'workshop_section',
+#         'no_of_participants')
+#     search_fields = (
+#         'expected_date',
+#         'workshop_level',
+#         'status')
+#     list_filter = ('is_active', 'status', 'workshop_level', 'location')
 
-admin.site.register(models.Workshop, WorkshopAdmin)
+admin.site.register(models.Workshop)
 
 
-class WorkshopFeedBackAdmin(admin.ModelAdmin):
-    pass
+# class WorkshopFeedBackAdmin(admin.ModelAdmin):
+#     pass
 
-admin.site.register(models.WorkshopFeedBack, WorkshopFeedBackAdmin)
+admin.site.register(models.WorkshopFeedBack)
 
 admin.site.register(models.WorkshopSections)
 admin.site.register(models.WorkshopRatingValues)
