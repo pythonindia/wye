90016be3ab4a53a31af9294fe3602bc268a0ccd8
vijay <vnbang2003@gmail.com>
2016-08-13 13:07:37 +0530

    Commented the workshop over write method due to circular import added datable
    Commented the workshop over write method due to circular import added
    datable plugin for upcoming_workshop page

--- wye/workshops/models.py
+++ wye/workshops/models.py
@@ -14,7 +14,7 @@ from wye.base.constants import (
 from wye.base.emailer_html import send_email_to_list
 from wye.base.models import TimeAuditModel
 from wye.organisations.models import Organisation
-from wye.profiles.models import Profile
+# from wye.profiles.models import Profile
 from wye.regions.models import Location
 
 from .decorators import validate_action_param, validate_assignme_action
@@ -56,42 +56,42 @@ class Workshop(TimeAuditModel):
     def __str__(self):
         return '{}-{}'.format(self.requester, self.workshop_section)
 
-    def save(self, force_insert=False, force_update=False, using=None):
-        """
-        send mail to region interested member whenever new workshop
-        is requested in respective region, therefore overriding
-        pre save method so mails are only send when new object
-        is created.
-        :param force_insert: bool force_insert
-        :param force_update: bool force_insert
-        :param using: using
-        :return: instance
-        """
-        if not self.id:
-            domain = Site.objects.get_current().domain
-            context = {
-                'workshop': self,
-                'date': self.expected_date,
-                'workshop_url': domain + '/workshop/{}/'.format(self.id)
-            }
-            # get region_interested_member email ids to notify them
-            region_interested_member = Profile.objects.filter(
-                interested_locations=self.requester.location,
-                usertype__slug='tutor'
-            ).values_list('user__email', flat=True)
+    # def save(self, force_insert=False, force_update=False, using=None):
+    #     """
+    #     send mail to region interested member whenever new workshop
+    #     is requested in respective region, therefore overriding
+    #     pre save method so mails are only send when new object
+    #     is created.
+    #     :param force_insert: bool force_insert
+    #     :param force_update: bool force_insert
+    #     :param using: using
+    #     :return: instance
+    #     """
+    #     if not self.id:
+    #         domain = Site.objects.get_current().domain
+    #         context = {
+    #             'workshop': self,
+    #             'date': self.expected_date,
+    #             'workshop_url': domain + '/workshop/{}/'.format(self.id)
+    #         }
+    #         # get region_interested_member email ids to notify them
+    #         region_interested_member = Profile.objects.filter(
+    #             interested_locations=self.requester.location,
+    #             usertype__slug='tutor'
+    #         ).values_list('user__email', flat=True)
 
-            subject = '[PythonExpress] Workshop request status.'
-            email_body = loader.get_template(
-                'email_messages/workshop/create_workshop/message.html').render(context)
-            text_body = loader.get_template(
-                'email_messages/workshop/create_workshop/message.txt').render(context)
-            send_email_to_list(
-                subject,
-                body=email_body,
-                users_list=region_interested_member,
-                text_body=text_body)
+    #         subject = '[PythonExpress] Workshop request status.'
+    #         email_body = loader.get_template(
+    #             'email_messages/workshop/create_workshop/message.html').render(context)
+    #         text_body = loader.get_template(
+    #             'email_messages/workshop/create_workshop/message.txt').render(context)
+    #         send_email_to_list(
+    #             subject,
+    #             body=email_body,
+    #             users_list=region_interested_member,
+    #             text_body=text_body)
 
-        super(Workshop, self).save(force_insert, force_update, using)
+    #     super(Workshop, self).save(force_insert, force_update, using)
 
 
     def is_presenter(self, user):
