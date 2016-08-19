3a04153c545f3c26ba39e0dbbd8d8f31b2d1ab53
shankisg <ssg.bnv@gmail.com>
2016-08-20 00:35:11 +0530

    fixed flake error

--- settings/common.py
+++ settings/common.py
@@ -221,7 +221,7 @@ TWITTER_CONSUMER_SECRET = os.environ.get("TWITTER_CONSUMER_SECRET", "")
 TWITTER_ACCESS_TOKEN = os.environ.get("TWITTER_ACCESS_TOKEN", "")
 TWITTER_ACCESS_TOKEN_SECRET = os.environ.get("TWITTER_ACCESS_TOKEN_SECRET", "")
 
-#google
+# Google
 GOOGLE_MAP_API = os.environ.get("GOOGLE_MAP_API", "")
 
 
