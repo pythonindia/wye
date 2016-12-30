a025d2520a3ead80fd147614858e2522c6dc89b7
shankisg <ssg.bnv@gmail.com>
2016-12-31 02:42:13 +0530

    added migration for volunteer, generated due to merge in migration

---
+++ wye/workshops/migrations/0011_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('workshops', '0009_workshopratingvalues_feedback_type'),
+        ('workshops', '0010_auto_20161022_1056'),
+    ]
+
+    operations = [
+    ]
