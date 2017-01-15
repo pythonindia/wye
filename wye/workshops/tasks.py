8bd31a2b897149b45846fc3808ce8ce2cc60e55a
Vijay <vijay@vijaybung.in>
2017-01-15 19:57:04 +0530

    Added Celery Test case and fixed bugs
    Fixed Issue #301 #353

--- wye/workshops/tasks.py
+++ wye/workshops/tasks.py
@@ -1,15 +1,47 @@
-from celery import task
 import datetime
 import os
+from celery import task
 
-from django.core.mail import EmailMessage, EmailMultiAlternatives
-from django.template import Context
-from django.template.loader import get_template
+from django.template import Context, loader
+# from django.template.loader import get_template
 
 from wye.workshops.models import Workshop
+from wye.base.emailer_html import send_email_to_id
 
 
 EMAIL_HOST = os.environ.get('EMAIL_HOST_USER', '')
+
+
+def intro_emails(organisation, email_context, email_id):
+    subject = '[PythonExpress] Intro for workshop at {}, {}'.format(
+        organisation.name, organisation.location.name)
+    email_body = loader.get_template(
+        'email_messages/workshop/workshop_intro_email.html').render(
+        email_context)
+    text_body = loader.get_template(
+        'email_messages/workshop/workshop_intro_email.txt').render(
+        email_context)
+    send_email_to_id(
+        subject,
+        body=email_body,
+        email_id=email_id,
+        text_body=text_body)
+
+
+def feedback_emails(organisation, email_context, email_id):
+    subject = '[PythonExpress] Request for Workshop Feedback {}, {}'.format(
+        organisation.name, organisation.location.name)
+    email_body = loader.get_template(
+        'email_messages/workshop/feedback_email.html').render(
+        email_context)
+    text_body = loader.get_template(
+        'email_messages/workshop/feedback_email.txt').render(
+        email_context)
+    send_email_to_id(
+        subject,
+        body=email_body,
+        email_id=email_id,
+        text_body=text_body)
 
 
 @task
@@ -18,100 +50,90 @@ def workshop_reminder(days, intro):
     send workshop reminder and intro email to all register user
     :return:
     """
-    template = get_template(
-        'email_messages/workshop/workshop_intro_email.html')
-    content = None
     workshop_date = datetime.date.today() + datetime.timedelta(days=days)
     workshops = Workshop.objects.select_related(
-        'workshop_section').filter(expected_date=workshop_date)
+        'workshop_section').filter(
+        expected_date=workshop_date).filter(
+        is_active=True)
 
     for workshop in workshops:
-        if workshop.is_active:
-            recipients = get_workshop_recipients(workshop)
-            presenters = workshop.presenter.all()
-            requesters = workshop.requester.user.all()
-            for r in requesters:
-                name = '{} {}'.format(r.first_name, r.last_name)
-                poc_phone = '{}'.format(r.profile.mobile)
-                poc_email = '{}'.format(r.email)
-            "\n".join('{}, {} ,{}' .format(r.first_name, for r in requesters)
-            if intro:
-                context_dict = {
-                    'date': workshop_date,
-                    'workshop_title': workshop.workshop_section.name,
-                    'poc_name': '',
-                    'poc_email': '',
-                    'poc_phone': '',
-                    'trainer_name': '',
-                    'trainer_email': '',
-                    'trainer_phone': ''
-                }
-                context = Context(context_dict)
-                content = template.render(context)
+        organisation = workshop.requester
 
-            for recipient in recipients:
-                # Reminder Email
-                message = "Hi, you have workshop scheduled for {workshop_date}.".format(
-                    workshop_date)
-                reminder_email = EmailMessage("[Workshop] Gentle Reminder",
-                                              message,
-                                              EMAIL_HOST,
-                                              [recipient])
-                reminder_email.send()
+        context_dict = {
+            'date': workshop_date,
+            'workshop_title': workshop.workshop_section.name,
+        }
+        if intro:
+            context_dict['poc_list'] = workshop.requester.user.all()
+            context_dict['presenter_list'] = workshop.presenter.all()
+            email_context = Context(context_dict)
+            # Below  loop is to send email individually
+            for requester in workshop.requester.user.all():
+                intro_emails(organisation, email_context, requester.email)
+            for presenter in workshop.presenter.all():
+                intro_emails(organisation, email_context, presenter.email)
+        else:
+            for presenter in workshop.presenter.all():
+                context_dict['poc_list'] = workshop.requester.user.all()
+                context_dict['presenter_list'] = workshop.presenter.all()
+                email_context = Context(context_dict)
+                subject = '[PythonExpress] Intro for workshop at {},{}'.format(
+                    organisation.name, organisation.location.name)
+                email_body = loader.get_template(
+                    'email_messages/workshop/workshop_intro_email.html').render(
+                    email_context)
+                text_body = loader.get_template(
+                    'email_messages/workshop/workshop_intro_email.txt').render(
+                    email_context)
+                send_email_to_id(
+                    subject,
+                    body=email_body,
+                    email_id=presenter.email,
+                    text_body=text_body)
 
-                # Intro Email
-                if intro:
-                    subject = 'Intro for workshop at {organisation_name}, {organisation_location}'
-                    intro_email = EmailMultiAlternatives(subject,
-                                                         from_email=EMAIL_HOST,
-                                                         to=[recipient],
-                                                         cc=['contact@pythonexpress.in '])
-                    intro_email.attach_alternative(content, 'text/html')
-                    intro_email.send()
+    return True
 
 
 @task
 def workshop_feedback(days=None):
     today = datetime.datetime.today()
     if days:
-        workshop_date = today - datetime.timedelta(days=2)
+        workshop_date = today + datetime.timedelta(days=2)
     workshops = Workshop.objects.filter(expected_date=workshop_date)
 
-    template = get_template('email_messages/workshop/feedback_email.html')
     for workshop in workshops:
-        if workshop.is_active:
-            recipients = get_workshop_recipients(workshop)
+        context_dict = {
+            'date': workshop_date,
+            'workshop_title': workshop.workshop_section.name,
+        }
+        email_context = Context(context_dict)
+        organisation = workshop.requester
+        for requester in workshop.requester.user.all():
+            feedback_emails(organisation, email_context, requester.email)
+        for presenter in workshop.presenter.all():
+            feedback_emails(organisation, email_context, presenter.email)
 
-            context = Context({'sender': 'pythonexpress.in '})
-            content = template.render(context)
-            for recipient in recipients:
-                subject = 'Python Express Workshop Feedback {organiser} {venue}'
-                feedback_email = EmailMultiAlternatives(subject,
-                                                        from_email=EMAIL_HOST,
-                                                        to=[recipient],
-                                                        cc=['contact@pythonexpress.in '])
-                feedback_email.attach_alternative(content, 'text/html')
-                feedback_email.send()
+    return True
 
 
-def get_workshop_recipients(workshop):
-    """
-    Filter out all recipients including requesters as well as presenters
-    :param workshop: workshop object
-    :return: list of all recipients w.r.t workshop
-    """
+# def get_workshop_recipients(workshop):
+#     """
+#     Filter out all recipients including requesters as well as presenters
+#     :param workshop: workshop object
+#     :return: list of all recipients w.r.t workshop
+#     """
 
-    presenter_email = list()
-    requester_email = list()
+#     presenter_email = list()
+#     requester_email = list()
 
-    requesters = workshop.requester.user.all()
-    for requester in requesters:
-        requester_email.append(requester.email)
+#     requesters = workshop.requester.user.all()
+#     for requester in requesters:
+#         requester_email.append(requester.email)
 
-    presenters = workshop.presenter.all()
-    for presenter in presenters:
-        presenter_email.append(presenter.email)
+#     presenters = workshop.presenter.all()
+#     for presenter in presenters:
+#         presenter_email.append(presenter.email)
 
-    recipients = presenter_email + requester_email
+#     recipients = presenter_email + requester_email
 
-    return recipients
+#     return recipients
