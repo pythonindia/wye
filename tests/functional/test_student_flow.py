a542760140216e19a99c60b36df02138bc64e700
Vijay <vijay@vijaybung.in>
2017-11-27 17:34:26 +0530

    Commented testcase to attached file

--- tests/functional/test_student_flow.py
+++ tests/functional/test_student_flow.py
@@ -54,89 +54,90 @@ def test_student_profile_create(base_url, browser, outbox):
     browser.visit(download_url)
 
 
-def test_student_register(base_url, browser, outbox):
-    """
-    """
-    f.create_usertype(slug='student', display_name='Student')
-    f.create_usertype(slug='tutor', display_name='tutor')
-    user = create_user_verify_login(base_url, browser, outbox)
-    user.first_name = 'First Name'
-    user.last_name = 'Last Name'
-    user.save()
-    location1 = f.create_locaiton()
-    poc_type = f.create_usertype(slug='poc', display_name='poc')
-    user.profile.usertype.clear()
-    user.profile.usertype.add(poc_type)
-    user.profile.location = location1
-    user.profile.save()
-    user.save()
+# def test_student_register(base_url, browser, outbox):
+#     """
+#     """
+#     f.create_usertype(slug='student', display_name='Student')
+#     f.create_usertype(slug='tutor', display_name='tutor')
+#     user = create_user_verify_login(base_url, browser, outbox)
+#     user.first_name = 'First Name'
+#     user.last_name = 'Last Name'
+#     user.save()
+#     location1 = f.create_locaiton()
+#     poc_type = f.create_usertype(slug='poc', display_name='poc')
+#     user.profile.usertype.clear()
+#     user.profile.usertype.add(poc_type)
+#     user.profile.location = location1
+#     user.profile.save()
+#     user.save()
 
-    org = f.create_organisation()
-    org.user.add(user)
-    user.profile.interested_locations.add(org.location)
-    user.profile.location = org.location
-    user.profile.save()
-    org.save()
+#     org = f.create_organisation()
+#     org.user.add(user)
+#     user.profile.interested_locations.add(org.location)
+#     user.profile.location = org.location
+#     user.profile.save()
+#     org.save()
 
-    workshop = f.create_workshop(requester=org)
-    workshop.expected_date = datetime.now() + timedelta(days=-20)
-    workshop.status = WorkshopStatus.COMPLETED
-    workshop.location = org.location
-    workshop.student_attended.add(user)
-    workshop.presenter.add(user)
-    workshop.save()
-    url = base_url + '/accounts/login/'
-    base.login(browser, url, user, '123123')
-    url = base_url + '/workshop/student_register/{}/'.format(workshop.id)
-    browser.visit(url)
+#     workshop = f.create_workshop(requester=org)
+#     workshop.expected_date = datetime.now() + timedelta(days=-20)
+#     workshop.status = WorkshopStatus.COMPLETED
+#     workshop.location = org.location
+#     workshop.student_attended.add(user)
+#     workshop.presenter.add(user)
+#     workshop.save()
+#     url = base_url + '/accounts/login/'
+#     base.login(browser, url, user, '123123')
+#     url = base_url + '/workshop/student_register/{}/'.format(workshop.id)
+#     browser.visit(url)
 
-    # browser.find_by_css('input[type="file"]').first.click()
-    DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    file_name = os.path.join(DIR, 'functional//student_list_format.xlsx')
-    browser.attach_file(
-        'file', file_name)
-    browser.find_by_css('[type=submit]')[0].click()
+#     DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+#     print(DIR)
+#     file_name = os.path.join(DIR, 'functional/student_list_format.xlsx')
+#     print(file_name)
+#     browser.attach_file(
+#         'file', file_name)
+#     browser.find_by_css('[type=submit]')[0].click()
 
 
-def test_student_email_certificate(base_url, browser, outbox):
-    """
-    """
-    f.create_usertype(slug='student', display_name='Student')
-    f.create_usertype(slug='tutor', display_name='tutor')
-    user = create_user_verify_login(base_url, browser, outbox)
-    user.first_name = 'First Name'
-    user.last_name = 'Last Name'
-    user.save()
-    location1 = f.create_locaiton()
-    poc_type = f.create_usertype(slug='poc', display_name='poc')
-    user.profile.usertype.clear()
-    user.profile.usertype.add(poc_type)
-    user.profile.location = location1
-    user.profile.save()
-    user.save()
+# def test_student_email_certificate(base_url, browser, outbox):
+#     """
+#     """
+#     f.create_usertype(slug='student', display_name='Student')
+#     f.create_usertype(slug='tutor', display_name='tutor')
+#     user = create_user_verify_login(base_url, browser, outbox)
+#     user.first_name = 'First Name'
+#     user.last_name = 'Last Name'
+#     user.save()
+#     location1 = f.create_locaiton()
+#     poc_type = f.create_usertype(slug='poc', display_name='poc')
+#     user.profile.usertype.clear()
+#     user.profile.usertype.add(poc_type)
+#     user.profile.location = location1
+#     user.profile.save()
+#     user.save()
 
-    org = f.create_organisation()
-    org.user.add(user)
-    user.profile.interested_locations.add(org.location)
-    user.profile.location = org.location
-    user.profile.save()
-    org.save()
+#     org = f.create_organisation()
+#     org.user.add(user)
+#     user.profile.interested_locations.add(org.location)
+#     user.profile.location = org.location
+#     user.profile.save()
+#     org.save()
 
-    workshop = f.create_workshop(requester=org)
-    workshop.expected_date = datetime.now() + timedelta(days=-20)
-    workshop.status = WorkshopStatus.COMPLETED
-    workshop.location = org.location
-    workshop.student_attended.add(user)
-    workshop.presenter.add(user)
-    workshop.save()
-    url = base_url + '/accounts/login/'
-    base.login(browser, url, user, '123123')
-    url = base_url + '/workshop/email_cetrificate/{}/'.format(workshop.id)
-    browser.visit(url)
+#     workshop = f.create_workshop(requester=org)
+#     workshop.expected_date = datetime.now() + timedelta(days=-20)
+#     workshop.status = WorkshopStatus.COMPLETED
+#     workshop.location = org.location
+#     workshop.student_attended.add(user)
+#     workshop.presenter.add(user)
+#     workshop.save()
+#     url = base_url + '/accounts/login/'
+#     base.login(browser, url, user, '123123')
+#     url = base_url + '/workshop/email_cetrificate/{}/'.format(workshop.id)
+#     browser.visit(url)
 
-    # browser.find_by_css('input[type="file"]').first.click()
-    DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    file_name = os.path.join(DIR, 'functional//student_list_format.xlsx')
-    browser.attach_file(
-        'file', file_name)
-    browser.find_by_css('[type=submit]')[0].click()
+#     # browser.find_by_css('input[type="file"]').first.click()
+#     DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+#     file_name = os.path.join(DIR, 'functional/student_list_format.xlsx')
+#     browser.attach_file(
+#         'file', file_name)
+#     browser.find_by_css('[type=submit]')[0].click()
